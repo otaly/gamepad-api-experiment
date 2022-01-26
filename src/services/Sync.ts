5fefde5cfbaa5455cd8e06a618aa037a3dc7d63b
naoki ota <on.s24jig@gmail.com>
2022-01-27 02:35:21 +0900

    ボタンを押し込む向き・距離を細かく設定できるよう変更

--- src/services/Sync.ts
+++ src/services/Sync.ts
@@ -2,31 +2,34 @@ import { GLTFResult } from 'src/models/Gamepad';
 import { getGamepad } from 'src/util/GetGamepad';
 import * as THREE from 'three';
 
+// アナログスティックの傾きの最大角
+const STICK_MAX_ANGLE = Math.PI / 9;
+
 export class Sync {
   static instance: Sync;
   private lastState: boolean[] | null = null;
-  nodes: THREE.Mesh[] = [];
-  private nodeMap: { [key in ModelNodeKeys]: THREE.Mesh | null } = {
-    x_btn: null,
-    circle_btn: null,
-    square_btn: null,
-    triangle_btn: null,
-    l_btn: null,
-    r_btn: null,
-    trigger_l: null,
-    trigger_r: null,
-    share_btn: null,
-    options_btn: null,
-    analog_stick_l: null,
-    analog_stick_r: null,
-    up_btn: null,
-    down_btn: null,
-    left_btn: null,
-    right_btn: null,
-    home_btn: null,
-    touchpad: null,
-    analog_stick_base: null,
-    body: null,
+  nodes: OperableModel[] = [];
+  private nodeMap: { [key in ModelNodeKeys]: OperableModel } = {
+    x_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    circle_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    square_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    triangle_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    l_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    r_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    trigger_l: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    trigger_r: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    share_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.02, z: 0 } },
+    options_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.02, z: 0 } },
+    analog_stick_l: { mesh: null, maxPressedPos: { x: 0, y: -0.04, z: 0 } },
+    analog_stick_r: { mesh: null, maxPressedPos: { x: 0, y: -0.04, z: 0 } },
+    up_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    down_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    left_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    right_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    home_btn: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    touchpad: { mesh: null, maxPressedPos: { x: 0, y: -0.06, z: 0 } },
+    analog_stick_base: { mesh: null },
+    body: { mesh: null },
   };
 
   constructor() {
@@ -39,37 +42,41 @@ export class Sync {
   setNodes(nodes: GLTFResult['nodes']) {
     this.nodes = [];
     Object.keys(this.nodeMap).forEach((key) => {
-      this.nodeMap[key as ModelNodeKeys] = null;
+      this.nodeMap[key as ModelNodeKeys].mesh = null;
       const node = nodes[key as ModelNodeKeys];
-      this.nodeMap[key as ModelNodeKeys] = node;
-      this.nodes.push(node);
+      this.nodeMap[key as ModelNodeKeys].mesh = node;
     });
+    this.nodes = Object.values(this.nodeMap);
   }
 
   update() {
     this.operateGamepadBtns((state, lastState, axes) => {
       state.forEach((pressed, index) => {
         const btnModel = this.nodes[index];
-        if (!btnModel) {
+        if (!btnModel.mesh || !btnModel.maxPressedPos) {
           return;
         }
         const wasPressed = lastState[index];
+        const { x, y, z } = btnModel.maxPressedPos;
         if (pressed && !wasPressed) {
-          btnModel.position.y -= 0.06;
+          btnModel.mesh.position.x += x;
+          btnModel.mesh.position.y += y;
+          btnModel.mesh.position.z += z;
         } else if (!pressed && wasPressed) {
-          btnModel.position.y += 0.06;
+          btnModel.mesh.position.x -= x;
+          btnModel.mesh.position.y -= y;
+          btnModel.mesh.position.z -= z;
         }
       });
 
-      const lStick = this.nodeMap.analog_stick_l;
-      const rStick = this.nodeMap.analog_stick_r;
-      const MAX_ANGLE = Math.PI / 9;
+      const lStick = this.nodeMap.analog_stick_l.mesh;
+      const rStick = this.nodeMap.analog_stick_r.mesh;
       [lStick, rStick].forEach((stick, index) =>
         // axesは[左スティックX軸, 左スティックY軸, 右スティックX軸, 右スティックY軸]の配列
         stick?.rotation.set(
-          -(axes[0 + index * 2] * MAX_ANGLE),
+          -(axes[0 + index * 2] * STICK_MAX_ANGLE),
           stick.rotation.y,
-          -(axes[1 + index * 2] * MAX_ANGLE)
+          -(axes[1 + index * 2] * STICK_MAX_ANGLE)
         )
       );
     });
@@ -94,3 +101,8 @@ export class Sync {
 }
 
 type ModelNodeKeys = keyof GLTFResult['nodes'];
+
+type OperableModel = {
+  mesh: THREE.Mesh | null;
+  maxPressedPos?: { x: number; y: number; z: number };
+};
