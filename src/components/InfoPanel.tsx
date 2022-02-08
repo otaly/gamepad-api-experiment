0acc9baa0c76d1cf7feef2db64d162e74d6b6fc8
naoki ota <on.s24jig@gmail.com>
2022-02-08 23:11:34 +0900

    インフォパネルの開閉実装

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getGamepad } from 'src/util/GetGamepad';
 
 const InfoPanel = () => {
-  const [{ gamepad }, setState] = useState({
+  const [{ gamepad, isOpen }, setState] = useState({
     gamepad: null as Gamepad | null,
+    isOpen: false,
   });
+
+  const infoArea = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     let reqAnimId: number;
@@ -20,46 +24,61 @@ const InfoPanel = () => {
     };
   }, []);
 
+  const arrow = isOpen ? '<<' : '>>';
+
+  const translateX = infoArea.current
+    ? `calc(${-infoArea.current.offsetWidth}px - 1rem)`
+    : '0px';
+
   return (
-    <div className="text-slate-300">
-      <p>Info</p>
-      {gamepad == null ? null : (
-        <>
-          <p>ID: {gamepad?.id}</p>
-          <p>Timestamp: {gamepad?.timestamp}</p>
-          <p>Mapping: {gamepad?.mapping}</p>
-          <table>
-            <tbody>
-              {gamepad?.buttons.map((b, i) => (
-                <tr key={i}>
-                  <td className="text-right">{i}.</td>
-                  <td>pressed: {b.pressed ? 'true' : 'false'},</td>
-                  <td>touched: {b.touched ? 'true' : 'false'},</td>
-                  <td>value: {b.value}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <ul>
-            {gamepad?.axes.map((axe, i) => (
-              <p key={i}>
-                axes[{i}]: {axe}
-              </p>
+    <div className="text-slate-300 inline-block">
+      <p
+        className="text-lg font-medium pointer-events-auto cursor-pointer mb-2"
+        onClick={() => setState((s) => ({ ...s, isOpen: !s.isOpen }))}
+      >
+        <span className="mr-2">{arrow}</span>
+        Info
+      </p>
+      <div
+        ref={infoArea}
+        className="transition-transform"
+        css={css({
+          transform: `translateX(${isOpen ? '0px' : translateX})`,
+        })}
+      >
+        <p>ID: {gamepad?.id}</p>
+        <p>Timestamp: {gamepad?.timestamp}</p>
+        <p>Mapping: {gamepad?.mapping}</p>
+        <table>
+          <tbody>
+            {gamepad?.buttons.map((b, i) => (
+              <tr className={b.pressed ? 'text-green-400' : ''} key={i}>
+                <td>pressed: {b.pressed ? 'true' : 'false'},</td>
+                <td>touched: {b.touched ? 'true' : 'false'},</td>
+                <td>value: {b.value}</td>
+              </tr>
             ))}
-          </ul>
-          <button
-            className="px-7 py-1 font-medium border rounded pointer-events-auto"
-            type="button"
-            onClick={() =>
-              gamepad
-                ? vibrate(gamepad, { duration: 1000, weakMagnitude: 0.5 })
-                : null
-            }
-          >
-            Vibrate
-          </button>
-        </>
-      )}
+          </tbody>
+        </table>
+        <ul>
+          {gamepad?.axes.map((v, i) => (
+            <p key={i}>
+              axes[{i}]: {v}
+            </p>
+          ))}
+        </ul>
+        <button
+          className="px-7 py-1 font-medium border rounded pointer-events-auto"
+          type="button"
+          onClick={() =>
+            gamepad
+              ? vibrate(gamepad, { duration: 1000, weakMagnitude: 0.5 })
+              : null
+          }
+        >
+          Vibrate
+        </button>
+      </div>
     </div>
   );
 };
