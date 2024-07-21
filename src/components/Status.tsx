340971a7aa207a2cc572e51b10dc4de034213831
naoki ota <otaly>
2024-07-22 04:15:57 +0900

    フォントサイズ調整

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react';
 import { useEffect, useState } from 'react';
 import { getGamepads } from 'util/GetGamepad';
 
@@ -43,13 +42,12 @@ const Status = () => {
 
   const status = isGamepadConnected ? (
     <p
-      className="text-green-400 tracking-wide text-4xl text-center animate-fadeout"
-      css={css({ animationDelay: '1s' })}
+      className="text-green-400 tracking-wide text-2xl sm:text-4xl text-center animate-fadeout animation-delay-1000"
     >
       Connected!!!
     </p>
   ) : (
-    <p className="text-white tracking-wide text-4xl text-center animate-pulse">
+    <p className="text-white tracking-wide text-2xl sm:text-4xl text-center animate-pulse">
       Connect your Gamepad...
     </p>
   );
